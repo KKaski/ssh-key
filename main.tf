ae73b120283a926189a65fa617a112a2bda9fc79
Kimmo Kaskikallio <kimmo.kaskikallio@fi.ibm.com>
2017-08-28 16:21:48 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,5 +68,5 @@ resource "ibm_compute_vm_instance" "my_server_1" {
 # Outputs
 ##############################################################################
 output "ssh_key_id" {
-  value = "${data.ibm_compute_ssh_key.public_key.id}"
+  value = "${ibm_compute_ssh_key.public_key.id}"
 }
