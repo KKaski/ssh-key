9c0d7f4faddd298722d3019f528b490aafbe1651
Kimmo Kaskikallio <kimmo.kaskikallio@fi.ibm.com>
2017-08-28 14:33:46 +0300

    Update server.tf

--- server.tf
+++ server.tf
@@ -55,3 +55,19 @@ variable key_note {
 output "ssh_key_id" {
   value = "${ibm_compute_ssh_key.ssh_key.id}"
 }
+
+##############################################################################
+# Create Servers
+##############################################################################
+# Create a virtual server with the SSH key.
+resource "ibm_compute_vm_instance" "my_server_1" {
+  hostname          = "host-b.example.com"
+  domain            = "example.com"
+  ssh_keys          = [123456, "${ibm_compute_ssh_key.ssh_key_1.id}"]
+  os_reference_code = "CENTOS_6_64"
+  datacenter        = "ams01"
+  network_speed     = 10
+  cores             = 1
+  memory            = 1024
+  hourly_billing    = true
+}
