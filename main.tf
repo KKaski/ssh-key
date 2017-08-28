ef1ad6d43e2df8eebbcb021bbb13632b09c5642d
Kimmo Kaskikallio <kimmo.kaskikallio@fi.ibm.com>
2017-08-28 15:08:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "ibm" {
 ##############################################################################
 # IBM SSH Key: For connecting to VMs
 ##############################################################################
-resource "ibm_compute_ssh_key" "ssh_key" {
+resource "ibm_compute_ssh_key" "test_ssh_key1" {
   label = "${var.key_label}"
   notes = "${var.key_note}"
   # Public key, so this is completely safe
@@ -30,7 +30,7 @@ resource "ibm_compute_ssh_key" "ssh_key" {
 resource "ibm_compute_vm_instance" "my_server_1" {
   hostname          = "host-b.example.com"
   domain            = "example.com"
-  ssh_keys          = [123456, "${ibm_compute_ssh_key.${var.key_label}.id}"]
+  ssh_keys          = [123456, "${ibm_compute_ssh_key.test_ssh_key1.id}"]
   os_reference_code = "CENTOS_6_64"
   datacenter        = "${var.datacenter}"
   network_speed     = 10
