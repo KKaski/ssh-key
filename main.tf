68ea6925b0d842b4839a33bc7dccd8c08e7e647c
Kimmo Kaskikallio <kimmo.kaskikallio@fi.ibm.com>
2017-08-28 15:39:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,3 +63,10 @@ resource "ibm_compute_vm_instance" "my_server_1" {
   memory            = 1024
   hourly_billing    = true
 }
+
+##############################################################################
+# Outputs
+##############################################################################
+output "ssh_key_id" {
+  value = "${ibm_compute_ssh_key.test_ssh_key1.id}"
+}
